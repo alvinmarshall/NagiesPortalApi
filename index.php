bd841f4588cb365bcf822b43639f5a35c4cef197
bik_krlvn <kelvinbirikorang@gmail.com>
2019-06-28 20:44:07 +0000

    relocated index file from public

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php
+<?php /** @noinspection PhpIncludeInspection */
 
 use App\Dispatcher;
-
-require_once "../src/app/bootstrap.php";
+require_once /** @lang text */
+"./src/app/bootstrap.php";
 header("Content-Type: application/json; charset UTF-8");
 $app = new Dispatcher();
 $app->dispatch();
