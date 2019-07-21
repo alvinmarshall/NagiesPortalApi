5dec060e8279e691e6d87855bf1bfc99e0b18d03
bik_krlvn <kelvinbirikorang@gmail.com>
2019-07-21 11:05:49 +0000

    added env

--- index.php
+++ index.php
@@ -5,6 +5,5 @@ use App\Dispatcher;
 require_once /** @lang text */
 "./src/app/bootstrap.php";
 header("Content-Type: application/json; charset UTF-8");
-//$app = new Dispatcher();
-//$app->dispatch();
-echo getenv('DB_NAME');
+$app = new Dispatcher();
+$app->dispatch();
