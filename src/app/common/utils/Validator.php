e89964dbc80bf061332b78ab0c66a71d3719da9d
bik_krlvn <kelvinbirikorang@gmail.com>
2019-07-31 16:42:12 +0000

    [update] added array data validation

--- src/app/common/utils/Validator.php
+++ src/app/common/utils/Validator.php
@@ -49,4 +49,30 @@ class Validator
         }
         return true;
     }
+
+    public static function isTypeAnArray($type, $field = 'field')
+    {
+        if (!is_array($type)) {
+            http_response_code(400);
+            echo json_encode(array('typeError' => "$field expected an array", 'status' => 400));
+            return false;
+        }
+        return true;
+    }
+
+    public static function validateArrayInput($arr, $field = 'field')
+    {
+        $error = [];
+        foreach ($arr as $key => $input) {
+            if (empty($input)) {
+                $error[$key] = "input can't be empty";
+            }
+        }
+        if (count($error) > 0) {
+            http_response_code(400);
+            echo json_encode([$field . 'Error' => $error, 'status' => 400]);
+            return false;
+        }
+        return true;
+    }
 }
